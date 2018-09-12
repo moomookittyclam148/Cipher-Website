7cc35153507ad5a7cde31b80970546beab4e459c
Miguel Angel Rodriguez <Marodriguez148@gmail.com>
2018-09-12 19:38:15 -0400

    Started encoding page, table set up, link to base64 set up

--- server.py
+++ server.py
@@ -12,11 +12,17 @@ def encodings():
 
 @app.route('/ciphers')
 def ciphers():
-        return render_template('ciphers.html')
+    return render_template('ciphers.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/encodings/<encoding>')
+def specific_encoding(encoding):
+    encoding_hash = {}
+    encoding_hash['base64'] = 'base64'
+    return render_template('converter.html', encoding_name=encoding_hash[encoding])
 
 if __name__ == '__main__':
    app.run(debug=True)
